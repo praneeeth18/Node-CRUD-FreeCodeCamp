62205cac43fecd3e6c30912e86f0a5979dae7db0
Praneeth Reddy <praneeth608@gmail.com>
2024-10-18 13:54:26 +0530

    update by id added

--- index.js
+++ index.js
@@ -42,6 +42,22 @@ app.post('/api/products',async (req, res) => {
     }
 });
 
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        
+        if(!product) {
+            return res.status(404).json({message: "Product not found!"});
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running at PORT: ${PORT}.`));
