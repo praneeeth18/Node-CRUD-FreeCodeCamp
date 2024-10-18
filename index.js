61a51dbfa36a24a5f17a1bdd15edcb75f667add8
Praneeth Reddy <praneeth608@gmail.com>
2024-10-18 13:00:43 +0530

    Mongodb database connected

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+const app = express();
+const PORT = process.env.PORT || 3500;
 
-app.listen(3000, () => {
-    console.log('Server started at port 3000');
-});
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server");
 })
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running at PORT: ${PORT}.`));
+});
