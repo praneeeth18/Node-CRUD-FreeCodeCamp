d0f1ee908c801fcfb44db1ef7c1d0451b92fa322
Praneeth Reddy <praneeth608@gmail.com>
2024-10-18 13:43:39 +0530

    post, getAll and getById added endpoints added

--- index.js
+++ index.js
@@ -2,14 +2,45 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
+const Product = require('./models/product.model.js')
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 connectDB();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server");
 })
+
+app.get('/api/products', async (req, res) => {
+   try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+   } catch (error) {
+        res.status(500).json({message: error.message});
+   } 
+});
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    } 
+});
+
+app.post('/api/products',async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
